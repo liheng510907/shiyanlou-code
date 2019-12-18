2281845b18f1711fa78698110ed8c278ab2705db
niuguisheshen <877473449@qq.com>
2019-12-18 20:59:00 +0800

    selenium

--- com/amap/com/amap.py
+++ com/amap/com/amap.py
@@ -1,1 +1,12 @@
-print("integration's")
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome()
+
+#browser = webdriver.Chrome()
+
+driver.get('https://www.baidu.com/index.php?tn=se_chromebtn')
+
+sleep(3)
+
+driver.quit()
